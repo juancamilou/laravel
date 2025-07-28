7e4c9943464272755bd7a493a0c2aa0bb1ed4fb3
juan <juancamilou07@gmail.com>
2025-07-27 23:19:34 -0500

    Se modica unicamente la inscripcion del rol ya que se me olvido un register para admin

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -28,7 +28,7 @@ class AuthController extends Controller
             $user = User::create([
                 'name' => $request->name,
                 'email' => $request->email,
-                'role' => 'student', 
+                'role' => 'admin', 
                 'password' => Hash::make($request->password),
             ]);
 
